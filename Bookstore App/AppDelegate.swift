54d7d8ec1e1bf5e7785a4cc86978804813e7a3b0
Maksims Salajevs <maximdtt@gmail.com>
2024-08-03 15:26:56 +0100

    preparing project for work

---
+++ Bookstore App/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Bookstore App
+//
+//  Created by Maksims Šalajevs on 03/08/2024.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        if let window = window {
+            window.rootViewController = UIViewController()
+        }
+        
+        return true
+    }
+
+}
+
