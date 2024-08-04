38a68d5eaa4cafacbc0fcd98cdeb079d810d1c8c
Maksims Salajevs <maximdtt@gmail.com>
2024-08-04 17:40:38 +0100

    home-screen

--- Bookstore App/Home/View/HomeCollectionViewCell.swift
+++ Bookstore App/Home/View/HomeCollectionViewCell.swift
@@ -13,12 +13,52 @@ final class HomeCollectionViewCell: UICollectionViewCell {
     
     // MARK: - GUI Variables
     
+    private lazy var imageView: UIImageView = {
+        let view = UIImageView()
+        
+        view.image = UIImage(named: "cover") ?? UIImage.add
+        view.contentMode = .scaleAspectFill
+        
+        return view
+    }()
+    
+    private lazy var categoryLabel: UILabel = {
+        let label = UILabel()
+        
+        label.text = "Fantasy"
+        label.textColor = .white
+        label.backgroundColor = .black
+        
+        return label
+    }()
+    
+    private lazy var nameOfAuthorLabel: UILabel = {
+        let label = UILabel()
+        
+        label.text = "J.K. Rowling"
+        label.textColor = .white
+        label.backgroundColor = .black
+        
+        return label
+    }()
+    
+    private lazy var nameOfBookLabel: UILabel = {
+        let label = UILabel()
+        
+        label.text = "Harry Potter and something"
+        label.numberOfLines = 0
+        label.textColor = .white
+        label.backgroundColor = .black
+        
+        return label
+    }()
     
     
     // MARK: - Initializations
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        setupUI()
     }
     
     required init?(coder: NSCoder) {
@@ -30,5 +70,31 @@ final class HomeCollectionViewCell: UICollectionViewCell {
     
     
     // MARK: - Private methods
+    
+    private func setupUI() {
+        [imageView, categoryLabel, nameOfBookLabel, nameOfAuthorLabel].forEach { addSubview($0) }
+        
+        setupConstraints()
+    }
+    
+    private func setupConstraints() {
+        
+        imageView.snp.makeConstraints { $0.size.edges.equalToSuperview() }
+        
+        categoryLabel.snp.makeConstraints {
+            $0.top.equalTo(imageView.snp.bottom)
+            $0.leading.trailing.bottom.equalToSuperview() }
+        
+        nameOfBookLabel.snp.makeConstraints {
+            $0.top.equalTo(categoryLabel.snp.bottom)
+            $0.leading.trailing.bottom.equalToSuperview()
+        }
+        
+        nameOfAuthorLabel.snp.makeConstraints {
+            $0.top.equalTo(nameOfBookLabel.snp.bottom)
+            $0.leading.trailing.bottom.equalToSuperview()
+        }
+        
+    }
     
 }
--- Bookstore App/Home/View/HomeViewController.swift
+++ Bookstore App/Home/View/HomeViewController.swift
@@ -16,8 +16,10 @@ class HomeViewController: UIViewController {
         let layout = UICollectionViewFlowLayout()
         layout.itemSize = CGSize(width: 176, height: 232)
         
-        let collectionView = UICollectionView(frame: CGRect(x: 0, y: 0, width: 365, height: 319), collectionViewLayout: layout)
+        let collectionView = UICollectionView(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height), collectionViewLayout: layout)
         
+        collectionView.dataSource = self
+        collectionView.delegate = self
         
         return collectionView
     }()
@@ -40,14 +42,39 @@ class HomeViewController: UIViewController {
         view.backgroundColor = .white
         view.addSubview(collectionView)
         
+        collectionView.register(HomeCollectionViewCell.self, forCellWithReuseIdentifier: HomeCollectionViewCell.reuseID)
+        
         setupConstraints()
     }
     
     private func setupConstraints() {
         collectionView.snp.makeConstraints { make in
-            make.top.leading.trailing.equalToSuperview()
+            make.top.leading.trailing.equalTo(view.safeAreaLayoutGuide)
         }
         
     }
 }
+
+// MARK: - UICollectionViewDataSource
+
+extension HomeViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: HomeCollectionViewCell.reuseID, for: indexPath) as? HomeCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        return cell
+    }
+}
+
+// MARK: - UICollectionViewDelegate
+
+extension HomeViewController: UICollectionViewDelegate {
+    
+}
+
+
 
